9b3e3574cd56e7a3052a3fc87168b8bb9cd8b56c
Thaís da Silva e Silva <thais.vieraa26@gmail.com>
2026-02-13 11:05:26 -0400

    Finishing login tests

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -16,6 +16,14 @@ test.describe("Fluxo completo de login", () => {
     await login.clicarOK();
   });
 
+  test ("Login com usuário inválido", async ({page}) => {
+    const login = new LoginPage (page);
+
+    await login.realizarLogin('thais.silva26@gmail.com', 'Thais@25');
+    await login.fecharModalErro();
+
+  })
+
   test("Login com apenas Usuário preenchido", async ({page}) => {
     const login = new LoginPage(page);
 
@@ -47,5 +55,19 @@ test.describe("Fluxo completo de login", () => {
     await login.realizarLogin(usuario, 'Thais@26');
     await login.fecharModalErro();
   });
-  
+
+  test('Login com senha com menos de 6 caracteres', async ({ page }) => {
+    const login = new LoginPage(page);
+
+    await login.realizarLogin('thais.teste26@gmail.com', 'Thais');
+    await login.fecharModalErro();
+  });
+
+  test('Login com senha com mais de 12 caracteres', async ({ page }) => {
+    const login = new LoginPage(page);
+
+    await login.realizarLogin('thais.teste26@gmail.com', 'Thais@26112343');
+    await login.fecharModalErro();
+  });
+
 });
